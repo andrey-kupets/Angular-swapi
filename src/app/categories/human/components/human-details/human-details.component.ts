08db8f61e1ea019ceb6d38b7a45fc8194a5535ae
andrey-kupets <andrey.kupets@i.ua>
2021-02-10 15:02:44 +0300

    add human.details comp-t

---
+++ src/app/categories/human/components/human-details/human-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-human-details',
+  templateUrl: './human-details.component.html',
+  styleUrls: ['./human-details.component.css']
+})
+export class HumanDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
