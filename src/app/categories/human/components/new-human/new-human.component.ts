d0f0b78bb13cc86f2bf5ce47868e83e50a1054b1
andrey-kupets <andrey.kupets@i.ua>
2021-02-17 02:07:18 +0300

    add creationForm

--- src/app/categories/human/components/new-human/new-human.component.ts
+++ src/app/categories/human/components/new-human/new-human.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Human} from '../../models/Human';
+import {FormBuilder, FormControl, FormGroup, FormsModule} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SubjectHumanService} from '../../services/subject-human.service';
 
 @Component({
   selector: 'app-new-human',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewHumanComponent implements OnInit {
 
-  constructor() { }
+  human: Human;
+  humanCreationForm: FormGroup;
+  id: number;
+
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private formsModule: FormsModule,
+              private formBuilder: FormBuilder,
+              private subjectHumanService: SubjectHumanService) {
+    this.activatedRoute.params.subscribe(value => {
+      this.human = this.router.getCurrentNavigation().extras.state as Human;
+    });
+  }
 
   ngOnInit(): void {
+    this.initForm();
+    this.id = +this.human.url.split('/').reverse()[1];
+  }
+
+  private initForm(): void {
+    this.humanCreationForm = this.formBuilder.group({
+      name: new FormControl(this.human.name),
+      height: new FormControl(this.human.height),
+      mass: new FormControl(this.human.mass),
+      hair_color: new FormControl(this.human.hair_color),
+      skin_color: new FormControl(this.human.skin_color),
+      eye_color: new FormControl(this.human.eye_color),
+      birth_year: new FormControl(this.human.birth_year),
+      gender: new FormControl(this.human.gender),
+      homeworld: new FormControl(this.human.homeworld),
+      created: new FormControl(this.human.created),
+      edited: new FormControl(this.human.edited),
+      url: new FormControl(this.human.url),
+    });
+  }
+
+  saveForm(humanEditionForm): void {
+    this.subjectHumanService.setNewHumanEditionContext(humanEditionForm.value);
+    console.log(humanEditionForm.value);
+    this.router.navigate(['people', this.id]);
   }
 
 }
